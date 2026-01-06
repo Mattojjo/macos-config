84dd956d02ace2061623ddefdb30e9d06cfec8ca
Lee <139267544+Mattojjo@users.noreply.github.com>
2026-01-06 14:26:51 -0500

    Changing nvim configs to match omarchy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,18 +1,2 @@
-vim.opt.termguicolors = true
--- Lazy.nvim bootstrap
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
---:NvimTreeToggle alias shortcut to Ctrl + n
-vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>")
-
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
